75dec7a75451621dc0df42f6f1f725e150d1c4cb
Danylo Hermanovych <elluyema@gmail.com>
2024-05-13 04:34:21 +0300

    fix build

--- src/auction/store/auctionsStore.ts
+++ src/auction/store/auctionsStore.ts
@@ -61,5 +61,5 @@ export const useAuctionsStore = defineStore('auction', () => {
         state.currentAuction.Rounds = rounds;
     };
 
-    return { state, loadAuctions, loadAuctionById, updateRounds };
+    return { state, updateRounds, loadAuctions, loadAuctionById };
 });
